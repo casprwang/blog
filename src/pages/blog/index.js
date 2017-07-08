32a20e587db685af89c8f6f20d6c42fa22aa0078
wangsongiam <wangsongiam@gmail.com>
2017-07-08 13:34:08 -0400

    something new

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,16 +2,31 @@ import React from "react"
 import Link from "gatsby-link"
 import Helmet from "react-helmet"
 
+
+const isLink = url => url.slice(0,7) === '/links/'
+
+
 export default props => {
   const posts = props.data.allMarkdownRemark.edges
   const siteTitle = props.data.site.siteMetadata.title
   const pathArray = props.data.allSitePage.edges
   const rows = []
+  const linkRows = []
 
   posts.forEach(post => {
     if (post.node.frontmatter.layout !== "page") {
-      if (post.node.frontmatter.draft === false) {
+      if (post.node.frontmatter.draft === false && !isLink(post.node.fields.slug)) {
         rows.push(
+          <section className="list" key={post.node.fields.slug}>
+            <h1>
+              <Link to={post.node.fields.slug}>
+                {post.node.frontmatter.title}
+              </Link>
+            </h1>
+          </section>
+        )
+      } else if (isLink(post.node.fields.slug)) {
+        linkRows.push(
           <section className="list" key={post.node.fields.slug}>
             <h1>
               <Link to={post.node.fields.slug}>
@@ -34,8 +49,11 @@ export default props => {
         <h1>Writing</h1>
         <small>TL;DR</small>
       </header>
-      <div className="page-content">
+      <div className="blog-content">
+        <h3>Today I learn</h3>
         {rows}
+        <h3>External Links</h3>
+        {linkRows}
       </div>
     </div>
   )
@@ -66,6 +84,8 @@ export const pageQuery = graphql`
           }
           frontmatter {
             title
+            link
+            tags
             date
             draft
             layout
