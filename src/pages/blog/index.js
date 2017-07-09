97086d1292c9793a982cd0a41266cfb86ab95b87
wangsongiam <wangsongiam@gmail.com>
2017-07-09 19:35:30 -0400

    reconstructed blog index for better looking

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -5,106 +5,98 @@ import Helmet from 'react-helmet'
 const isLink = url => url.slice(0, 7) === '/links/'
 const isTIL = url => url.slice(0, 5) === '/til/'
 
+const BlogArray = props =>
+	<section className="list" key={props.slug}>
+		<h1>
+			<Link to={props.slug}>
+				{props.title}
+			</Link>
+		</h1>
+	</section>
+
+const pushArray = arr => {
+	arr.push(<BlogArray slug={slug} title={title} />)
+}
+
 export default props => {
-  const posts = props.data.allMarkdownRemark.edges
-  const siteTitle = props.data.site.siteMetadata.title
-  const pathArray = props.data.allSitePage.edges
-  const rows = []
-  const linkRows = []
-  const TILRows = []
+	const posts = props.data.allMarkdownRemark.edges
+	const siteTitle = props.data.site.siteMetadata.title
+	const pathArray = props.data.allSitePage.edges
+	const rows = []
+	const linkRows = []
+	const TILRows = []
 
-  posts.forEach(post => {
-    if (post.node.frontmatter.layout !== 'page') {
-      if (!isLink(post.node.fields.slug)) {
-        if (isTIL(post.node.fields.slug)) {
-          TILRows.push(
-            <section className="list" key={post.node.fields.slug}>
-              <h1>
-                <Link to={post.node.fields.slug}>
-                  {post.node.frontmatter.title}
-                </Link>
-              </h1>
-            </section>,
-          )
-        } else {
-          rows.push(
-            <section className="list" key={post.node.fields.slug}>
-              <h1>
-                <Link to={post.node.fields.slug}>
-                  {post.node.frontmatter.title}
-                </Link>
-              </h1>
-            </section>,
-          )
-        }
-      } else if (isLink(post.node.fields.slug)) {
-        linkRows.push(
-          <section className="list" key={post.node.fields.slug}>
-            <h1>
-              <Link to={post.node.fields.slug}>
-                {post.node.frontmatter.title}
-              </Link>
-            </h1>
-          </section>,
-        )
-      }
-    }
-  })
+	posts.forEach(post => {
+		let slug = post.node.fields.slug
+		let title = post.node.frontmatter.title
 
-  return (
-    <div className="content">
-      <Helmet
-        title={`Writing`}
-        meta={[{ name: 'description', content: "Song Wang's Writings" }]}
-      />
-      <header>
-        <h1>Writing</h1>
-        <h2>TL;DR</h2>
-      </header>
-      <div className="blog-content">
-        <h3>Today I learned</h3>
-        {TILRows}
-        <h3>Blogs</h3>
-        {rows}
-        <h3>External Links</h3>
-        {linkRows}
-      </div>
-    </div>
-  )
+		if (post.node.frontmatter.layout !== 'page') {
+			if (!isLink(slug)) {
+				if (isTIL(slug)) {
+					TILRows.push(<BlogArray slug={slug} title={title} />)
+				} else {
+					rows.push(<BlogArray slug={slug} title={title} />)
+				}
+			} else if (isLink(slug)) {
+					linkRows.push(<BlogArray slug={slug} title={title} />)
+			}
+		}
+	})
+
+	return (
+		<div className="content">
+			<Helmet
+				title={`Writing`}
+				meta={[{ name: 'description', content: "Song Wang's Writings" }]}
+			/>
+			<header>
+				<h1>Writing</h1>
+				<h2>TL;DR</h2>
+			</header>
+			<div className="blog-content">
+				<h3>Today I learned</h3>
+				{TILRows}
+				<h3>Blogs</h3>
+				{rows}
+				<h3>External Links</h3>
+				{linkRows}
+			</div>
+		</div>
+	)
 }
 
 export const pageQuery = graphql`
-  query BlogQuery {
-    allSitePage {
-      edges {
-        node {
-          path
-        }
-      }
-    }
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(
-      limit: 1000
-      sort: { fields: [frontmatter___date], order: DESC }
-    ) {
-      edges {
-        node {
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            link
-            tags
-            date
-            layout
-          }
-        }
-      }
-    }
-  }
+	query BlogQuery {
+		allSitePage {
+			edges {
+				node {
+					path
+				}
+			}
+		}
+		site {
+			siteMetadata {
+				title
+			}
+		}
+		allMarkdownRemark(
+			limit: 1000
+			sort: { fields: [frontmatter___date], order: DESC }
+		) {
+			edges {
+				node {
+					fields {
+						slug
+					}
+					frontmatter {
+						title
+						link
+						tags
+						date
+						layout
+					}
+				}
+			}
+		}
+	}
 `
