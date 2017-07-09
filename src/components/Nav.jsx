16666065dd756032445119f78e2a186454c96c39
wangsongiam <wangsongiam@gmail.com>
2017-07-08 20:47:30 -0400

    fixed visited coloring issue with link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,40 +1,42 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const paths = ['/','/blog/','/about/', '/hire/']
+const paths = ['/', '/blog/', '/about/', '/hire/']
 
 const Li = props => {
-  let style = (props.location === props.current)? {color: '#a212d1'} : {color:'#000'}
-  {/* console.log(props.location === props.current) */}
-  console.log(props.location.length>1)
-  if (props.location.length>1){
+  let style
+  if (
+    props.current.split('/')[1] ===
+    props.location.slice(1, props.location.length - 1)
+  ) {
+    style = { color: '#a212d1' }
+  } else {
+    style = { color: '#000' }
+  }
+  if (props.location.length > 1) {
     return (
       <li>
-        <Link 
-          style={style}
-          to={`${props.location}`}
-        >
-          {props.location.slice(1, props.location.length-1)}
+        <Link style={style} to={`${props.location}`}>
+          {props.location.slice(1, props.location.length - 1)}
         </Link>
       </li>
-      )
-    } else {
+    )
+  } else {
     return (
       <li>
-        <Link 
-          style={style}
-          to={`${props.location}`}
-        >
+        <Link style={style} to={`${props.location}`}>
           {'Home'}
         </Link>
       </li>
-      )
-      }
+    )
+  }
 }
 
 export default props => {
-  let links = paths.map(path=><Li location={path} current={props.location.pathname}/>)
-  
+  let links = paths.map(path =>
+    <Li location={path} current={props.location.pathname} />,
+  )
+
   return (
     <nav>
       <ul>
