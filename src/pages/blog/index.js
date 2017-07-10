10da53f5db8880dc78d643f98ec12479c4367de2
wangsongiam <wangsongiam@gmail.com>
2017-07-10 18:43:18 -0400

    sync

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,9 +2,6 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-const isLink = url => url.slice(0, 7) === '/links/'
-const isTIL = url => url.slice(0, 5) === '/til/'
-
 const BlogArray = props =>
 	<section className="list" key={props.slug}>
 		<h1>
@@ -14,10 +11,6 @@ const BlogArray = props =>
 		</h1>
 	</section>
 
-const pushArray = arr => {
-	arr.push(<BlogArray slug={slug} title={title} />)
-}
-
 export default props => {
 	const posts = props.data.allMarkdownRemark.edges
 	const siteTitle = props.data.site.siteMetadata.title
@@ -25,48 +18,48 @@ export default props => {
 	const rows = []
 	const linkRows = []
 	const TILRows = []
-  const notesRows = []
+	const notesRows = []
 
-  /*
+	/*
      ['til', 'notes', 'links', 'blogs(else)']
   */
 	posts.forEach(post => {
 		let slug = post.node.fields.slug
 		let title = post.node.frontmatter.title
-    switch(slug.split('/')[1]){
-      case 'til':
-        TILRows.push(<BlogArray slug={slug} title={title} />)
-        break
-      case 'blog':
-        rows.push(<BlogArray slug={slug} title={title} />)
-        break
-      case 'links':
-        linkRows.push(<BlogArray slug={slug} title={title} />)
-        break
-      case 'notes':
-        console.log('notes')
-        notesRows.push(<BlogArray slug={slug} title={title} />)
-        break
-      default:
-        break
-    }
+		switch (slug.split('/')[1]) {
+			case 'til':
+				TILRows.push(<BlogArray slug={slug} title={title} />)
+				break
+			case 'blog':
+				rows.push(<BlogArray slug={slug} title={title} />)
+				break
+			case 'links':
+				linkRows.push(<BlogArray slug={slug} title={title} />)
+				break
+			case 'notes':
+				console.log('notes')
+				notesRows.push(<BlogArray slug={slug} title={title} />)
+				break
+			default:
+				break
+		}
 	})
 
 	return (
 		<div className="content">
 			<Helmet
-				title={`Writing`}
+				title={`Blog`}
 				meta={[{ name: 'description', content: "Song Wang's Writings" }]}
 			/>
 			<header>
-				<h1>Writing</h1>
+				<h1>Blog</h1>
 				<h2>TL;DR</h2>
 			</header>
 			<div className="blog-content">
 				<h3>Today I learned</h3>
 				{TILRows}
-        <h3>Notes</h3>
-        {notesRows}
+				<h3>Notes</h3>
+				{notesRows}
 				<h3>Blogs</h3>
 				{rows}
 				<h3>External Links</h3>
