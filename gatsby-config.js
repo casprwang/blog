4c16ad20f71de25d7328809340929952aace2dcd
wangsongiam <wangsongiam@gmail.com>
2017-07-11 13:14:03 -0400

    added static image folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/static/images/`,
         name: "images"
       }
     },
