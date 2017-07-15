b3e107c707ca22a637e90e5c6bde6d3f93214436
wangsongiam <wangsongiam@gmail.com>
2017-07-15 13:03:50 -0400

    added prop-types

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -3,104 +3,103 @@ import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
 const BlogArray = props =>
-	<section className="list" key={props.slug}>
-		<h1>
-			<Link to={props.slug}>
-				{props.title}
-			</Link>
-		</h1>
-	</section>
+  <section className="list" key={props.slug}>
+    <h1>
+      <Link to={props.slug}>
+        {props.title}
+      </Link>
+    </h1>
+  </section>
 
 export default props => {
-	const posts = props.data.allMarkdownRemark.edges
-	const siteTitle = props.data.site.siteMetadata.title
-	const pathArray = props.data.allSitePage.edges
-	const rows = []
-	const linkRows = []
-	const TILRows = []
-	const notesRows = []
+  const posts = props.data.allMarkdownRemark.edges
+  const siteTitle = props.data.site.siteMetadata.title
+  const pathArray = props.data.allSitePage.edges
+  const rows = []
+  const linkRows = []
+  const TILRows = []
+  const notesRows = []
 
-	/*
+  /*
      ['til', 'notes', 'links', 'blogs(else)']
   */
-	posts.forEach(post => {
-		let slug = post.node.fields.slug
-		let title = post.node.frontmatter.title
-		switch (slug.split('/')[1]) {
-			case 'til':
-				TILRows.push(<BlogArray slug={slug} title={title} />)
-				break
-			case 'blog':
-				rows.push(<BlogArray slug={slug} title={title} />)
-				break
-			case 'links':
-				linkRows.push(<BlogArray slug={slug} title={title} />)
-				break
-			case 'notes':
-				console.log('notes')
-				notesRows.push(<BlogArray slug={slug} title={title} />)
-				break
-			default:
-				break
-		}
-	})
+  posts.forEach(post => {
+    let slug = post.node.fields.slug
+    let title = post.node.frontmatter.title
+    switch (slug.split('/')[1]) {
+      case 'til':
+        TILRows.push(<BlogArray slug={slug} title={title} />)
+        break
+      case 'blog':
+        rows.push(<BlogArray slug={slug} title={title} />)
+        break
+      case 'links':
+        linkRows.push(<BlogArray slug={slug} title={title} />)
+        break
+      case 'notes':
+        notesRows.push(<BlogArray slug={slug} title={title} />)
+        break
+      default:
+        break
+    }
+  })
 
-	return (
-		<div className="content">
-			<Helmet
-				title={`Blog`}
-				meta={[{ name: 'description', content: "Song Wang's Writings" }]}
-			/>
-			<header>
-				<h1>Blog</h1>
-				<h2>TL;DR</h2>
-			</header>
-			<div className="blog-content">
-				<h3>Today I learned</h3>
-				{TILRows}
-				<h3>Notes</h3>
-				{notesRows}
-				<h3>Blogs</h3>
-				{rows}
-				<h3>External Links</h3>
-				{linkRows}
-			</div>
-		</div>
-	)
+  return (
+    <div className="content">
+      <Helmet
+        title={`Blog`}
+        meta={[{ name: 'description', content: "Song Wang's Writings" }]}
+      />
+      <header>
+        <h1>Blog</h1>
+        <h2>TL;DR</h2>
+      </header>
+      <div className="blog-content">
+        <h3>Today I learned</h3>
+        {TILRows}
+        <h3>Notes</h3>
+        {notesRows}
+        <h3>Blogs</h3>
+        {rows}
+        <h3>External Links</h3>
+        {linkRows}
+      </div>
+    </div>
+  )
 }
 
 export const pageQuery = graphql`
-	query BlogQuery {
-		allSitePage {
-			edges {
-				node {
-					path
-				}
-			}
-		}
-		site {
-			siteMetadata {
-				title
-			}
-		}
-		allMarkdownRemark(
-			limit: 1000
-			sort: { fields: [frontmatter___date], order: DESC }
-		) {
-			edges {
-				node {
-					fields {
-						slug
-					}
-					frontmatter {
-						title
-						link
-						tags
-						date
-						layout
-					}
-				}
-			}
-		}
-	}
+  query BlogQuery {
+    allSitePage {
+      edges {
+        node {
+          path
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      limit: 1000
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            link
+            tags
+            date
+            layout
+          }
+        }
+      }
+    }
+  }
 `
