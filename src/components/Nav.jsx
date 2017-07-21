59667982a69f568005ede60e7a465eeb7274b9e9
wangsongiam <wangsongiam@gmail.com>
2017-07-21 01:03:28 -0400

    changed nav to class component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,38 +4,73 @@ import styled from 'styled-components'
 
 const paths = ['/', '/blog/', '/about/', '/hire/']
 
-const Li = props => {
-  let style
-  if (
-    props.current.split('/')[1] ===
-    props.location.slice(1, props.location.length - 1)
-  ) {
-    style = { color: '#a212d1' }
-  } else {
-    style = { color: '#000' }
+class Li extends React.Component {
+  constructor(props) {
+    super()
   }
-  if (props.location.length > 1) {
-    return (
-      <li>
-        <Link style={style} to={`${props.location}`}>
-          {props.location.slice(1, props.location.length - 1)}
-        </Link>
-      </li>
-    )
-  } else {
-    return (
-      <li>
-        <Link style={style} to={`${props.location}`}>
-          {'Home'}
-        </Link>
-      </li>
-    )
+
+  render() {
+    let style
+    if (
+      this.props.current.split('/')[1] ===
+      this.props.location.slice(1, this.props.location.length - 1)
+    ) {
+      style = { color: '#a212d1' }
+    } else {
+      style = { color: '#000' }
+    }
+    if (this.props.location.length > 1) {
+      return (
+        <li>
+          <Link style={style} to={`${this.props.location}`}>
+            {this.props.location.slice(1, this.props.location.length - 1)}
+          </Link>
+        </li>
+      )
+    } else {
+      return (
+        <li>
+          <Link style={style} to={`${this.props.location}`}>
+            {'Home'}
+          </Link>
+        </li>
+      )
+    }
   }
 }
 
+// const Li = props => {
+//   let style
+//   if (
+//     props.current.split('/')[1] ===
+//     props.location.slice(1, props.location.length - 1)
+//   ) {
+//     style = { color: '#a212d1' }
+//   } else {
+//     style = { color: '#000' }
+//   }
+//   if (props.location.length > 1) {
+//     return (
+//       <li>
+//         <Link style={style} to={`${props.location}`}>
+//           {props.location.slice(1, props.location.length - 1)}
+//         </Link>
+//       </li>
+//     )
+//   } else {
+//     return (
+//       <li>
+//         <Link style={style} to={`${props.location}`}>
+//           {'Home'}
+//         </Link>
+//       </li>
+//     )
+//   }
+// }
+
 export default props => {
   let links = paths.map(path =>
-    <Li location={path} current={props.location.pathname} />,
+    <Li location={path} current={props.location.pathname} />
   )
 
   return (
