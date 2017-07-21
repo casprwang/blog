79991579b23925a3bbe740b0cf9a4f04f5120629
wangsongiam <wangsongiam@gmail.com>
2017-07-21 02:00:40 -0400

    nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,40 +4,85 @@ import styled from 'styled-components'
 
 const paths = ['/', '/blog/', '/about/', '/hire/']
 
-const Li = props => {
-  let style
-  if (
-    props.current.split('/')[1] ===
-    props.location.slice(1, props.location.length - 1)
-  ) {
-    style = { color: '#a212d1' }
-  } else {
-    style = { color: '#000' }
+class Li extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hover: false
+      }
   }
-  if (props.location.length > 1) {
-    return (
-      <li>
-        <Link
-          style={style}
-          activeStyle={{
-            color: '#a212d1'
-          }}
-          to={`${props.location}`}
-        >
-          {props.location.slice(1, props.location.length - 1)}
-        </Link>
-      </li>
-    )
-  } else {
-    return (
-      <li>
-        <Link style={style} to={`${props.location}`}>
-          {'Home'}
-        </Link>
-      </li>
-    )
+
+  _toggleHover() {
+    this.setState({
+      hover: !this.state.hover
+      })
+    }
+
+  render() {
+    let style
+    this.props.current.split('/')[1] ===
+    this.props.location.slice(1, this.props.location.length - 1)
+      ? (style = { color: '#a212d1' })
+      : (style = { color: '#000' })
+    if (this.props.location.length > 1) {
+      return (
+        <li>
+          <Link
+            onMouseEnter={()=>this._toggleHover()}
+            activeStyle={{
+              color: '#a212d1'
+            }}
+            to={`${this.props.location}`}
+          >
+            {this.props.location.slice(1, this.props.location.length - 1)}
+          </Link>
+        </li>
+      )
+    } else {
+      return (
+        <li>
+          <Link 
+            onMouseEnter={()=>this._toggleHover()}
+            style={style} 
+            to={`${this.props.location}`}>
+            {'Home'}
+          </Link>
+        </li>
+      )
+    }
   }
 }
+
+// const Li = props => {
+//   let style
+//   props.current.split('/')[1] ===
+//   props.location.slice(1, props.location.length - 1)
+//     ? (style = { color: '#a212d1' })
+//     : (style = { color: '#000' })
+//   if (props.location.length > 1) {
+//     return (
+//       <li>
+//         <Link
+//           style={style}
+//           activeStyle={{
+//             color: '#a212d1'
+//           }}
+//           to={`${props.location}`}
+//         >
+//           {props.location.slice(1, props.location.length - 1)}
+//         </Link>
+//       </li>
+//     )
+//   } else {
+//     return (
+//       <li>
+//         <Link style={style} to={`${props.location}`}>
+//           {'Home'}
+//         </Link>
+//       </li>
+//     )
+//   }
+// }
 
 export default props => {
   let links = paths.map(path =>
