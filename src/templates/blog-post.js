a88b22d3e69afa844034e6879ef4151eaa06704e
wangsongiam <wangsongiam@gmail.com>
2017-07-29 04:55:44 -0400

    new linking method for posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -8,86 +8,42 @@ class BlogPostRoute extends React.Component {
     const post = this.props.data.markdownRemark
     const { fields: { slug } } = post
 
-    if (post.frontmatter.layout === 'page') {
-      return (
-        <div>
-          <Helmet
-            title={`${post.frontmatter.title}`}
-            meta={[{ name: 'description', content: post.excerpt }]}
-          />
-          <div className="content">
-            <header>
-              <h1>
-                {post.frontmatter.title}
-              </h1>
-            </header>
-            <Content dangerouslySetInnerHTML={{ __html: post.html }} />
-            <hr />
-            <TagSection {...this.props} />
-          </div>
-        </div>
-      )
-    } else if (slug.slice(5, 12) === '/links/') {
-      return (
-        <div>
-          <Helmet
-            title={`${post.frontmatter.title}`}
-            meta={[
-              {
-                name: 'description',
-                content: post.excerpt || post.frontmatter.description
-              }
-            ]}
-          />
-          <div className="content">
-            <header>
-              <a
-                rel="noopener noreferrer"
-                target="__blank"
-                href={post.frontmatter.link}
-              >
-                <h1>
+    return (
+      <div>
+        <Helmet
+          title={`${post.frontmatter.title}`}
+          meta={[
+            {
+              name: 'description',
+              content: post.excerpt || post.frontmatter.description
+            }
+          ]}
+        />
+        <div className="content">
+          <header>
+            {slug.slice(5, 12) === '/links/'
+              ? <a
+                  rel="noopener noreferrer"
+                  target="__blank"
+                  href={post.frontmatter.link}
+                >
+                  <h1>
+                    {post.frontmatter.title}
+                  </h1>
+                </a>
+              : <h1>
                   {post.frontmatter.title}
-                </h1>
-              </a>
-              <small>
-                {post.frontmatter.date}
-              </small>
-            </header>
-            <Content dangerouslySetInnerHTML={{ __html: post.html }} />
-            <hr />
-            <TagSection {...this.props} />
-          </div>
+                </h1>}
+            <small>
+              {post.frontmatter.date}
+            </small>
+          </header>
+          <Content dangerouslySetInnerHTML={{ __html: post.html }} />
+          <hr />
+          <TagSection {...this.props} />
         </div>
-      )
-    } else {
-      return (
-        <div>
-          <Helmet
-            title={`${post.frontmatter.title}`}
-            meta={[
-              {
-                name: 'description',
-                content: post.excerpt || post.frontmatter.description
-              }
-            ]}
-          />
-          <div className="content">
-            <header>
-              <h1>
-                {post.frontmatter.title}
-              </h1>
-              <small>
-                {post.frontmatter.date}
-              </small>
-            </header>
-            <Content dangerouslySetInnerHTML={{ __html: post.html }} />
-            <hr />
-            <TagSection {...this.props} />
-          </div>
-        </div>
-      )
-    }
+      </div>
+    )
   }
 }
 
