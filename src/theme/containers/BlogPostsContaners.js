e17cfdb80717d5b16c4c235eca10f1f0ce57bbac
wangsongiam <wangsongiam@gmail.com>
2017-09-02 16:02:02 -0400

    reset content typography

--- src/theme/containers/BlogPostsContaners.js
+++ src/theme/containers/BlogPostsContaners.js
@@ -3,12 +3,12 @@ import { media } from 'theme/utils/media'
 
 const Base = styled.div`
   h2 {
-    font-size: 1.3em;
+    font-size: 1.62rem;
     margin-top: 2em;
   }
 
   h3 {
-    font-size: 1em;
+    font-size: 1.424rem;
     margin-top: .5em;
   }
 
