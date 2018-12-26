bdcaccd9515d1d67b4dec7cabc17793cc04d4c43
wangsongiam <wangsongiam@gmail.com>
2018-12-25 21:50:26 -0800

    used relative path

--- gatsby-node.js
+++ gatsby-node.js
@@ -110,7 +110,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 exports.onCreateWebpackConfig = ({ stage, actions }) => {
   actions.setWebpackConfig({
     resolve: {
-      modules: [path.resolve(__dirname, "src"), "node_modules"],
+      modules: ["src", "node_modules"],
     },
   })
 }
