91524af8cec7c9f864d7bf0a5c1048069393d856
wangsongiam <wangsongiam@gmail.com>
2018-12-25 21:25:13 -0800

    added icon logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,7 +81,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/logo.png`,
       },
     },
     `gatsby-plugin-offline`,
